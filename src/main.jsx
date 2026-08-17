15679ac379f5eb154b30f95f2273c10b1da66d1b
sirakinb <sirakinb@gmail.com>
2026-08-17 13:53:32 -0400

    Load planetary header styling

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { createRoot, hydrateRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
+import "./planetary.css";
 
 // Register service worker for caching
 if ('serviceWorker' in navigator) {
